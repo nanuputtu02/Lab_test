7a7910263cac71d75c7a82a6d1b8768654982bad
nandanr <canulovemenow9164@gmail.com>
2026-03-12 12:10:27 +0530

    Add additional print statement in Madam.java

--- Madam.java
+++ Madam.java
@@ -2,5 +2,6 @@ class Madam{
 public static void main(String [] args){
   System.out.println("hello deer");
   System.out.println("SYALLABUS======>stress");
+  System.out.println("boys are beautiful with thoughts");
 }
 }
